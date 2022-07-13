3a4e6bb25796e07160dbb9127acae0c7e1808a6d
Pancake-Q <pancakeqi.code@gmail.com>
2022-07-13 21:17:08 +0800

    feat:comment

--- src/demo/type/comment.ts
+++ src/demo/type/comment.ts
@@ -96,3 +96,44 @@ const todoInfo: TodoInfo = {
 };
 // const todoInfo: TodoInfo
 todoInfo;
+
+/**
+ * Extract<Type, Union>:通过从 Type 中提取可分配给 Union 的所有 union 成员，构造一个类型。
+ */
+// type T0 = "a"
+type T0 = Extract<'a' | 'b' | 'c', 'a' | 'f'>;
+// type T1 = () => void
+type T1 = Extract<string | number | (() => void), Function>;
+
+/**
+ * NonNullable:通过从 Type 中排除 null 和undefined 来构造一个类型。
+ */
+// type T0 = string | number
+type T0 = NonNullable<string | number | undefined>;
+// type T1 = string[]
+type T1 = NonNullable<string[] | null | undefined>;
+/**
+ * Parameters:从一个函数类型 Type 的参数中使用的类型构建一个元组类型。
+ */
+declare function f1(arg: { a: number; b: string }): void;
+// type T0 = []
+type T0 = Parameters<() => string>;
+// type T1 = [s: string]
+type T1 = Parameters<(s: string) => void>;
+// type T2 = [arg: unknown]
+type T2 = Parameters<<T>(arg: T) => T>;
+/*
+ type T3 = [arg: {
+ a: number;
+ b: string;
+ }]
+ */
+type T3 = Parameters<typeof f1>;
+// type T4 = unknown[]
+type T4 = Parameters<any>;
+// type T5 = never
+type T5 = Parameters<never>;
+// type T6 = never
+type T6 = Parameters<string>;
+// type T7 = never
+type T7 = Parameters<Function>;
